3315fb3607367ed44dd0e5ff8b92f6f3cab86405
clara-almeida <clara.almeidagom@gmail.com>
2024-06-12 11:09:57 -0300

    push11

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Concede permissões de execução ao script
+chmod +x run.sh
+
 # Construa a imagem Docker
-docker build -t meu_projeto .
+docker build -t meu_projeto 
 
 # Execute o contêiner Docker
 docker run -d -p 8080:80 meu_projeto
